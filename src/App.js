ffea3a3fe169274877a0a5d5114f34dc8da02677
Cristian Araujo <cristian.jesus@zup.com.br>
2020-04-19 20:31:16 -0300

    Criado component de Balance

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Header from './components/Header';
 
 import './App.css';
+import Balance from './components/Balance';
 
 function App() {
   return (
     <div>
       <Header />
+      <Balance />
     </div>
   );
 }
