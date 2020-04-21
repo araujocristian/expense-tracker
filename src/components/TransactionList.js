b989f8761fdf7d3bec84597db1455fdf3a88a09c
Cristian Araujo <cristian.jesus@zup.com.br>
2020-04-21 10:50:25 -0300

    Add Context

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 import "./TransactionList.css";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li className="minus" key={transaction.id}>
+            {transaction.text} <span>{`R$ ${transaction.amount}`}</span>
+            <button className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   );
